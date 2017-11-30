eae700d51fce7be1bad733ff8d30ac8ebb1ba054
penright <paul@enrights.net>
2017-11-30 17:09:52 -0600

    Add files via upload
    Only the ap_home was changed, the rest was add because the date/time was updated from running the batch file.

--- OpenGarage/html_sta_home.h
+++ OpenGarage/html_sta_home.h
@@ -1,17 +1,17 @@
 const char html_sta_home[] PROGMEM = R"(<body>
-  <style> table, th, td {border: 0px solid black;padding: 6px; border-collapse: collapse; }</style>
-  <div data-role='page' id='page_home'><div data-role='header'><h3 id='head_name'>OG</h3></div>
-  <div data-role='content'><div data-role='fieldcontain'>
-  <table><tr><td><b>Door&nbsp;State:<br></td><td><label id='lbl_status'>-</label></td>
-  <td rowspan="2"><img id='pic' src='' style='width:112px;height:64px;'></td>
-  </tr><tr><td><b><label id='lbl_vstatus1'>Vehicle&nbsp;State:&nbsp</label></b></td>
-  <td><label id='lbl_vstatus'>-</label></td></tr>
-   <tr><td><b>Distance:</b></td><td><label id='lbl_dist'>-</label></td><td></td></tr>
-  <tr><td><b>Read&nbsp;Count:</b></td><td><label id='lbl_beat'>-</label></td><td></td></tr>
-  <tr><td><b>WiFi&nbsp;Signal:</b></td><td colspan="2"><label id='lbl_rssi'>-</label></td></tr>
-  <tr><td><b>Device&nbsp;Key:</b></td><td colspan="2" ><input type='password' size=20 maxlength=32 name='dkey' id='dkey'></td></tr>
-  <tr><td colspan=3><label id='msg'></label></td></tr>
-  </table><br />
+<style> table, th, td {border: 0px solid black;padding: 6px; border-collapse: collapse; }</style>
+<div data-role='page' id='page_home'><div data-role='header'><h3 id='head_name'>OG</h3></div>
+<div data-role='content'><div data-role='fieldcontain'>
+<table><tr><td><b>Door&nbsp;State:<br></td><td><label id='lbl_status'>-</label></td>
+<td rowspan="2"><img id='pic' src='' style='width:112px;height:64px;'></td>
+</tr><tr><td><b><label id='lbl_vstatus1'>Vehicle&nbsp;State:&nbsp</label></b></td>
+<td><label id='lbl_vstatus'>-</label></td></tr>
+<tr><td><b>Distance:</b></td><td><label id='lbl_dist'>-</label></td><td></td></tr>
+<tr><td><b>Read&nbsp;Count:</b></td><td><label id='lbl_beat'>-</label></td><td></td></tr>
+<tr><td><b>WiFi&nbsp;Signal:</b></td><td colspan="2"><label id='lbl_rssi'>-</label></td></tr>
+<tr><td><b>Device&nbsp;Key:</b></td><td colspan="2" ><input type='password' size=20 maxlength=32 name='dkey' id='dkey'></td></tr>
+<tr><td colspan=3><label id='msg'></label></td></tr>
+</table><br />
 <div data-role='controlgroup' data-type='horizontal'>
 <button data-theme='b' id='btn_click'>Button</button>  
 <button data-theme='b' id='btn_opts'>Options</button>
@@ -65,59 +65,59 @@ $('#msg').html('Device is now in AP mode. Log on<br>to SSID OG_xxxxxx, then <br>
 }
 });  
 $('#btn_click').click(function(e) {
-    show_msg('Sending command.....',5000,'green');
-    navigator.vibrate([500]);
-    var comm = 'cc?click=1&dkey='+($('#dkey').val());
-    $.getJSON(comm)
-      .done(function( jd ) {
-        if(jd.result!=1) {
-          show_msg('Check device key and try again.',2000,'red');
-        }else{clear_msg();};
-      })
-      .fail(function( jqxhr, textStatus, error ) {
-        var err = error;
-        $('#msg').text('Request Failed: ' + err).css('color','red');
-      });
-    });
+show_msg('Sending command.....',5000,'green');
+navigator.vibrate([500]);
+var comm = 'cc?click=1&dkey='+($('#dkey').val());
+$.getJSON(comm)
+.done(function( jd ) {
+if(jd.result!=1) {
+show_msg('Check device key and try again.',2000,'red');
+}else{clear_msg();};
+})
+.fail(function( jqxhr, textStatus, error ) {
+var err = error;
+$('#msg').text('Request Failed: ' + err).css('color','red');
+});
+});
 $(document).ready(function() {
 show(); si=setInterval('show()', 5000);
 });
 function show() {
-  $.getJSON('jc', function(jd) {
-  $('#fwv').text('v'+(jd.fwv/100>>0)+'.'+(jd.fwv/10%10>>0)+'.'+(jd.fwv%10>>0));
-  $('#lbl_dist').text(jd.dist +' (cm)').css('color', jd.dist==450?'red':'black');
-  $('#lbl_status').text(jd.door?'OPEN':'CLOSED').css('color',jd.door?'red':'green'); 
-  //Hide or Show vehicle info
-  if (jd.vehicle >=2){ //2 is unknown, 3 disabled
-    $('#lbl_vstatus1').hide();
-    $('#lbl_vstatus').text('');
-  }else {
-    $('#lbl_vstatus1').show()
-    $('#lbl_vstatus').text(jd.vehicle?'Present':'Absent');
-  }
-  //Open Door (1)
-  if (jd.door){
-    if (jd.vehicle>=3){ //3 is disabled
-      $('#pic').attr('src', '/DoorOpen.png');
-    }else if (jd.vehicle==2){
-      $('#pic').attr('src', '/OpenUnknown.png');  
-    }else{
-      $('#pic').attr('src', jd.vehicle?'/OpenPresent.png':'/OpenAbsent.png');  
-    }
-  }else{ //Closed Door 
-    if (jd.vehicle>=3){ //3 is disabled
-      $('#pic').attr('src', '/DoorShut.png');
-    }else if (jd.vehicle==2){
-      $('#pic').attr('src', '/ClosedUnknown.png');  //This should be impossible
-    }else{
-      $('#pic').attr('src', jd.vehicle?'/ClosedPresent.png':'/ClosedAbsent.png');  
-    }
-  }
-  $('#lbl_beat').text(jd.rcnt);
-  $('#lbl_rssi').text((jd.rssi>-71?'Good':(jd.rssi>-81?'Weak':'Poor')) +' ('+ jd.rssi +' dBm)');
-  $('#head_name').text(jd.name);
-  $('#btn_click').html(jd.door?'Close Door':'Open Door').button('refresh');
-  });
-  }
+$.getJSON('jc', function(jd) {
+$('#fwv').text('v'+(jd.fwv/100>>0)+'.'+(jd.fwv/10%10>>0)+'.'+(jd.fwv%10>>0));
+$('#lbl_dist').text(jd.dist +' (cm)').css('color', jd.dist==450?'red':'black');
+$('#lbl_status').text(jd.door?'OPEN':'CLOSED').css('color',jd.door?'red':'green'); 
+//Hide or Show vehicle info
+if (jd.vehicle >=2){ //2 is unknown, 3 disabled
+$('#lbl_vstatus1').hide();
+$('#lbl_vstatus').text('');
+}else {
+$('#lbl_vstatus1').show()
+$('#lbl_vstatus').text(jd.vehicle?'Present':'Absent');
+}
+//Open Door (1)
+if (jd.door){
+if (jd.vehicle>=3){ //3 is disabled
+$('#pic').attr('src', '/DoorOpen.png');
+}else if (jd.vehicle==2){
+$('#pic').attr('src', '/OpenUnknown.png');  
+}else{
+$('#pic').attr('src', jd.vehicle?'/OpenPresent':'/OpenAbsent');  
+}
+}else{ //Closed Door 
+if (jd.vehicle>=3){ //3 is disabled
+$('#pic').attr('src', '/DoorShut.png');
+}else if (jd.vehicle==2){
+$('#pic').attr('src', '/ClosedUnknown.png');  //This should be impossible
+}else{
+$('#pic').attr('src', jd.vehicle?'/ClosedPresent':'/ClosedAbsent');  
+}
+}
+$('#lbl_beat').text(jd.rcnt);
+$('#lbl_rssi').text((jd.rssi>-71?'Good':(jd.rssi>-81?'Weak':'Poor')) +' ('+ jd.rssi +' dBm)');
+$('#head_name').text(jd.name);
+$('#btn_click').html(jd.door?'Close Door':'Open Door').button('refresh');
+});
+}
 </script>
 </body>)";
