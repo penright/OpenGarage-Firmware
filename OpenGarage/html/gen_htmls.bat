701524a5e8c731f57005162ad93b2fc6a0e44acf
penright <paul@enrights.net>
2017-11-30 08:16:01 -0600

    Add files via upload
    So that html header files could be created in a windows environment. 
    gen_htmls.bat is ported from gen_htmls.sh
    html2ESP_Win.exe is html2esp.cpp compiled with vs 2017.

---
+++ OpenGarage/html/gen_htmls.bat
@@ -0,0 +1,7 @@
+.\html2ESP_Win.exe ap_home.html html_ap_home
+pause
+.\html2ESP_Win.exe sta_home.html html_sta_home
+.\html2ESP_Win.exe sta_options.html html_sta_options
+.\html2ESP_Win.exe sta_update.html html_sta_update
+.\html2ESP_Win.exe sta_logs.html html_sta_logs
+pause
